a7ee48b4d4725981be2557c4a8efe463382d845a
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-05-10 20:16:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## 作成したテンプレートについて
+
+```shell
+>npx create-next-app ./
+√ Would you like to use TypeScript? ... No / Yes #Yes
+√ Would you like to use ESLint? ... No / Yes #Yes
+√ Would you like to use Tailwind CSS? ... No / Yes #No
+√ Would you like to use `src/` directory? ... No / Yes #Yes
+√ Would you like to use App Router? (recommended) ... No / Yes #Yes
+√ Would you like to customize the default import alias (@/*)? ... No / Yes #No
+```
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "jsys24 ハッカソン テンプレート",
+  description: "jsys24ハッカソンのテンプレートです。",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
